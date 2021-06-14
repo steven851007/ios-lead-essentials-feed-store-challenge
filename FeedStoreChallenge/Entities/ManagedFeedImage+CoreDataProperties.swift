bad835ffc3c956221c948f8da77a1bba20cac36d
Istvan Balogh <istvan.balogh@outlook.com>
2021-06-15 00:10:22 +0700

    Restore inverser relationship to fix warning

--- FeedStoreChallenge/Entities/ManagedFeedImage+CoreDataProperties.swift
+++ FeedStoreChallenge/Entities/ManagedFeedImage+CoreDataProperties.swift
@@ -20,6 +20,7 @@ public class ManagedFeedImage: NSManagedObject {
 	@NSManaged public var descriptionString: String?
 	@NSManaged public var location: String?
 	@NSManaged public var url: URL
+	@NSManaged public var cache: ManagedCache?
 }
 
 extension ManagedFeedImage: Identifiable {}
